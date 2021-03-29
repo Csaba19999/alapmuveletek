a6540a6742afb456aed5265bf2b9177ef7020950
CsabaAdmin <menesi777@gmail.com>
2021-03-29 11:10:19 +0200

    kozosGUI01Terv

---
+++ src/pkg2021/pkg03/pkg19_kozosgui01/terv/Terv.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2021.pkg03.pkg19_kozosgui01.terv;
+
+/**
+ *
+ * @author Csaba
+ */
+public class Terv {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
